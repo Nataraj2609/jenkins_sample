1b13d7a834a56c0542a2b84df71b4fed3d13532c
Nataraj M (I21014) <nataraj.manivannan@ideas2it.com>
2021-04-06 13:26:44 +0530

    log added

--- src/main/java/com/jenkins/Sample/SampleApplication.java
+++ src/main/java/com/jenkins/Sample/SampleApplication.java
@@ -1,13 +1,19 @@
 package com.jenkins.Sample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SampleApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(SampleApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Application started");
 		SpringApplication.run(SampleApplication.class, args);
+		logger.info("Application Stopped :) ");
 	}
 
 }
